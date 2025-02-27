5c11334ca0ec89e130b3fdc2e74c070494c72f4e
thanhdnh <myhoangthanh@yahoo.com>
2025-02-27 13:40:54 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+public class Book{
+    public string title;
+    public string author;
+    public long price;
+    public Book(string title, string author, long price){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+}
+public class Books:DictionaryBase{
+    public void Add(string key, Book value){
+        base.InnerHashtable.Add(key, value);
+    }
+    public void Remove(string key){
+        base.InnerHashtable.Remove(key);
+    }
+    public Book Items(string key){
+        return (Book)base.InnerHashtable[key];
+    }
+}
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
